f618ae4552e8bca372c90e1c4d236da278ae5408
Alex Schoof <alex.schoof@gmail.com>
2010-02-23 12:16:10 -0500

    Added an exception
    Added a KeyLengthError exception in encoder to handle the case where the length of the key is shorter than the length of the text

--- encoder.py
+++ encoder.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 
+class KeyLengthError (Exception):
+	def __str__(self):
+		return "ERROR: Key shorter than text!"
+
 def encrypt(plaintext, key):
 	if len(key) < len(plaintext):
-		key = (key*2)[:len(plaintext)]
+	#	key = (key*2)[:len(plaintext)]
+		raise KeyLengthError
 	result = ''
 	for i in range(len(plaintext)):
 		result += chr((((ord(plaintext[i])-32) + (ord(key[i])-32)) % 95) + 32)
 	return result
 
 def decrypt(ciphertext, key):
+	if len(key) < len(ciphertext):
+		raise KeyLengthError
 	result = ''
 	for i in range(len(ciphertext)):
 		result += chr((((ord(ciphertext[i])-32) - (ord(key[i])-32)) % 95) + 32)
